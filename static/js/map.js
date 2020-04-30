c14a482fafddf3e070ff1314e38a1504ed06ee23
Chi Yu <cfyu1998@gmail.com>
2020-04-29 22:22:11 -0400

    added basic map framework with us states data

---
+++ static/js/map.js
@@ -0,0 +1,13 @@
+// creates map
+var mapboxAccessToken = "pk.eyJ1IjoiY2Z5dSIsImEiOiJjazlpMW8zazgxNGJ4M2ZvNGZ4c3BnaDk2In0.w2voJd0D3iz6s6KjouJ9pg";
+var map = L.map('mapid').setView([37.8, -96], 4);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
+    id: 'mapbox/light-v9',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
+
+// adds states data to the map
+L.geoJson(statesData).addTo(map);
