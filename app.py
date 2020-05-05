c2baf0d7318ff9feb8e1fba8f6591f01ffd61cab
mjiang9 <mayjiang@princeton.edu>
2020-05-05 13:32:14 -0400

    newsapi

--- app.py
+++ app.py
@@ -4,12 +4,23 @@ from pymongo import MongoClient
 import json
 from bson import json_util
 from bson.json_util import dumps
+from newsapi import NewsApiClient
+
+# Init
+newsapi = NewsApiClient(api_key='92f7976f22e94e109f47ef929d205515')
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/news")
+def getnews():
+    top_headlines = newsapi.get_top_headlines(q='california',
+                                              language='en',
+                                              country='us')
+    return top_headlines
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
--- static/js/map.js
+++ static/js/map.js
@@ -10,4 +10,64 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(map);
 
 // adds states data to the map
-L.geoJson(statesData).addTo(map);
+// L.geoJson(statesData).addTo(map);
+
+var geojson;
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
